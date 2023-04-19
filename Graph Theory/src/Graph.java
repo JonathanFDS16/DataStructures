d39fc490977122280b90303cfa38e1eb982b5e86
JonathanFDS16 <jonathanfranco799@gmail.com>
2023-04-19 16:02:57 -0400

    Setting up Project and JUnit testing.

---
+++ Graph Theory/src/Graph.java
@@ -0,0 +1,29 @@
+public class Graph {
+
+    public boolean addNode(String name) {
+        return false;
+    }
+
+    public boolean addNodes(String[] names) {
+        return false;
+    }
+
+    public boolean addEdge(String from, String to) {
+        return false;
+    }
+
+    public boolean addEdges(String from, String[] toList) {
+        return false;
+    }
+
+    public boolean removeNode(String name) {
+        return false;
+    }
+
+    public boolean removeNodes(String[] nodes) {
+        return false;
+    }
+
+    public void printGraph() {
+    }
+}
