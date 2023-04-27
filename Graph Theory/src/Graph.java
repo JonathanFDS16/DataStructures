9fc732557bdfa17aefb2a35d1e4ba53f2af938e7
JonathanFDS16 <jonathanfranco799@gmail.com>
2023-04-27 12:43:26 -0400

    Creation of all methods and testing

--- Graph Theory/src/Graph.java
+++ Graph Theory/src/Graph.java
@@ -1,29 +1,207 @@
-public class Graph {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Hashtable;
+import java.util.LinkedList;
 
-    public boolean addNode(String name) {
+public class Graph<K, V> {
+
+    public class Node {
+        private K name;
+        private V value;
+
+        private LinkedList<Node> neighbors = new LinkedList<>();
+
+        public Node(K name, V value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        private K getName() {
+            return name;
+        }
+
+        private V getValue() {
+            return value;
+        }
+
+        private void addNeighbor(Node neighbor) {
+            neighbors.add(neighbor);
+        }
+
+        private LinkedList<Node> getNeighbors() {
+            return neighbors;
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append(this.getName() + " ");
+            neighbors.sort((node1, node2) -> {
+                if (node1.getName() instanceof String) {
+                    String name1 = (String) node1.getName();
+                    String name2 = (String) node2.getName();
+                    return name1.compareTo(name2);
+                }
+                else return 0;
+            });
+            for (Node n : neighbors) {
+                builder.append(n.getName() + " ");
+            }
+            return builder.toString();
+        }
+    }
+
+    // Thinking about hashTable
+    private Hashtable<K, Node> nodesTable;
+
+    private ArrayList<K> keys = new ArrayList<>();
+
+    public Graph() {
+        nodesTable = new Hashtable<>();
+    }
+
+    public boolean addNode(K name, V value) {
+        // Check duplicates and then add
+        if (nodesTable.containsKey(name))
+            return false;
+
+        // Otherwise add the node to the hashtable
+        keys.add(name);
+        Node temp = new Node(name, value);
+        nodesTable.put(name, temp);
+        return true;
+    }
+
+    public boolean addNodes(K[] names, V[] data) {
+        boolean wasAllAdded = true;
+
+        if (names.length != data.length)
+            throw new IllegalArgumentException();
+
+        // Loop goes checks for duplicates and adds nodes with keys not in hashTable
+        for (int i = 0; i < names.length; i++) {
+           if (!nodesTable.containsKey(names[i])) {
+               keys.add(names[i]);
+               nodesTable.put(names[i], new Node(names[i], data[i]));
+           }
+           else wasAllAdded = false;
+        }
+        return wasAllAdded;
+    }
+
+    public boolean addEdge(K from,K to) {
+        // I need to check if both exist
+        if (nodesTable.containsKey(from) && nodesTable.containsKey(to)
+                && !isConnected(from, to)) {
+            // Adds the neighbor for the node with K from
+            nodesTable.get(from).addNeighbor(nodesTable.get(to));
+            // Adds the neighbor to the node with K to.
+            nodesTable.get(to).addNeighbor(nodesTable.get(from));
+            return true;
+        }
         return false;
     }
 
-    public boolean addNodes(String[] names) {
-        return false;
+    public boolean addEdges(K from, K[] toList) {
+        boolean check = true;
+        // Check is K from exist
+        if (!nodesTable.containsKey(from))
+            return false;
+
+        for (K element : toList) {
+            if (nodesTable.containsKey(element) && !isConnected(from, element)) {
+                nodesTable.get(from).addNeighbor(nodesTable.get(element));
+                nodesTable.get(element).addNeighbor(nodesTable.get(from));
+            }
+            else check = false;
+        }
+        return check;
     }
 
-    public boolean addEdge(String from, String to) {
-        return false;
+    public boolean removeNode(K name) {
+        if (!nodesTable.containsKey(name))
+            return false;
+
+        removeEdges(name);
+        return true;
     }
 
-    public boolean addEdges(String from, String[] toList) {
-        return false;
+    private void removeEdges(K name) {
+        Node temp = nodesTable.get(name);
+        if (temp.getNeighbors().isEmpty()) {
+            keys.remove(name);
+            nodesTable.remove(name);
+            return;
+        }
+
+        Node first = temp.getNeighbors().getFirst();
+        first.getNeighbors().remove(temp);
+        temp.getNeighbors().remove(0);
+        removeEdges(name);
     }
 
-    public boolean removeNode(String name) {
-        return false;
-    }
-
-    public boolean removeNodes(String[] nodes) {
-        return false;
+    public boolean removeNodes(K[] nodeList) {
+        boolean check = true;
+        for (int i = 0; i < nodeList.length; i++) {
+            if (nodesTable.containsKey(nodeList[i])) {
+                removeEdges(nodeList[i]);
+            }
+            else check = false;
+        }
+        return check;
     }
 
     public void printGraph() {
+        for (K name : keys) {
+            Node node = nodesTable.get(name);
+            System.out.println(node.toString());
+        }
+    }
+
+    public static <V> Graph<String, V> read(String filename) throws FileNotFoundException {
+        Graph<String, V> graph = new Graph<>();
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        // For each line of the text file
+        reader.lines().forEach( line -> {
+            StringBuilder nodeName = new StringBuilder();   // stores the nodeName being read in line
+            boolean firstNode = true;                       // keeps track of the first word of the line
+            String firstNodeName = null;                    // saves the name of the firstNode
+            // Loop through line to do operations in each word found
+            for (int i = 0; i < line.length(); i++) {
+                nodeName.append(line.charAt(i));            // Append the character to the nodeName
+                // If next char is a whitespace or current char is the last of the line
+                if (i + 1 == line.length() ||  Character.isWhitespace(line.charAt(i + 1))) {
+                    if (firstNode) {
+                        firstNode = false;
+                        firstNodeName = nodeName.toString();
+                        graph.addNode(firstNodeName, null);
+                    } else {
+                        // If graph does not contain the node/neighbor --> add node to graph
+                        if (!graph.containsNode(nodeName.toString()))
+                            graph.addNode(nodeName.toString(), null);
+                        // If the firstNode is not connected to its neighbor --> add an edge between them
+                        if (!graph.isConnected(firstNodeName, nodeName.toString()))
+                            graph.addEdge(firstNodeName, nodeName.toString());
+                    }
+                }
+                nodeName.delete(0, Integer.MAX_VALUE);          // Reset the nodeName for the next one
+            }
+        });
+        return graph;
+    }
+
+    private boolean containsNode(K name) {
+        return nodesTable.containsKey(name);
+    }
+
+    private boolean isConnected(K from, K to) {
+        if (nodesTable.containsKey(from) && nodesTable.containsKey(to)) {
+           if (nodesTable.get(from).getNeighbors().contains(nodesTable.get(to)))
+               return true;
+           else return false;
+        }
+        return false;
     }
 }
--- Graph Theory/test/GraphTest.java
+++ Graph Theory/test/GraphTest.java
@@ -1,9 +1,131 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GraphTest {
 
+    /**
+     * Testing addNode()
+     * Tests the method with the following possibilities
+     * 1. Add a node that does not exist
+     * 2. Add a node that exist
+     *
+     * Testing addNode(arrays)
+     * Tests the method with the following possibilities
+     * 1. Name array and data array are not equal length
+     * 2. Add arrays with all unique keys, therefore, all nodes does not exist
+     * 3. Add arrays with some repeated keys, meaning some nodes are repeated
+     * @throws FileNotFoundException
+     */
     @Test
-    public void test(){}
+    public void addNodeTest() throws FileNotFoundException {
+        Graph<String, Integer> graph = new Graph<>();
+        assertTrue(graph.addNode("A", 1));
+        assertTrue(graph.addNode("B", 2));
+        assertFalse(graph.addNode("A", 2));
+
+        // Testing addNodes()
+        Graph<String, Integer> graph1 = new Graph<>();
+        String[] keys = {"A", "B", "C", "D", "F"};
+        Integer[] values = {1, 2, 3, 1, 2};
+        String[] keys1 = {"A", "A", "C", "C", "F"};
+        Integer[] values1 = {1, 2, 3, 1, 2};
+        Integer[] values2 = {1, 2, 3, 1, 2, 6};
+        assertTrue(graph1.addNodes(keys, values));
+        assertFalse(graph1.addNodes(keys1, values1));
+        assertThrows( IllegalArgumentException.class,
+                () -> {
+                    graph1.addNodes(keys, values2);
+                }
+        );
+    }
+
+    /**
+     * Testing addEdge()
+     * Method is tested with the following possibilities:
+     * 1. Add an edge between two unconnected vertices
+     * 2. Add an edge between two vertices but one of them does not exist
+     * 3. Add an edge that already exist between two vertices
+     * 4. Try to add an edge but Node 'from' does not exist
+     */
+    @Test
+    public void addEdgeTest() {
+        Graph<String, Integer> graph = createGraphWithNodes();
+        assertTrue(graph.addEdge("A", "B"));
+        assertTrue(graph.addEdge("A", "C"));
+        assertFalse(graph.addEdge("B", "A"));
+        assertFalse(graph.addEdge("B", "Z"));
+        assertFalse(graph.addEdge("Z", "A"));
+        graph.printGraph();
+
+        Graph<String, Integer> graph1 = createGraphWithNodes();
+        String[] toList = {"B", "C", "D", "F"};
+        String[] toList1 = {"B", "B", "D", "D"};
+        String[] toList2 = {"B", "C", "Z", "F"};
+        assertTrue(graph1.addEdges("A", toList));
+        assertFalse(graph1.addEdges("A", toList1));
+        assertFalse(graph1.addEdges("B", toList2));
+        assertFalse(graph1.addEdges("Z", toList));
+
+    }
+
+    /**
+     * Testing removeNode()
+     * Method is tested with the following possibilities
+     * 1. Remove a node that does not exist
+     * 2. Remove a node that exist
+     *      --> Node without any connection
+     *      --> Node with connection
+     * Testing removeNodes(arrays)
+     * Methods is tested with the following possibilities
+     * 1. Remove nodes that does not exist
+     * 2. Remove nodes that all exist
+     *      --> Nodes without connections
+     *      --> Nodes with connections
+     */
+    @Test
+    public void removeNodeTest() {
+        Graph<String, Integer> graph = createGraphWithConnectedNodes();
+        String[] nodeListNonExist = {"A", "B", "Z"};
+        String[] nodeList = {"C", "F"};
+        assertFalse(graph.removeNode("Z"));
+        assertTrue(graph.removeNode("G"));
+        assertTrue(graph.removeNode("A"));
+        assertTrue(graph.removeNodes(nodeList));
+        assertFalse(graph.removeNodes(nodeListNonExist));
+    }
+
+    @Test
+    public void printGraphTest() {
+        Graph<String, Integer> graph = createGraphWithNodes();
+        graph.addEdge("A", "F");
+        graph.addEdge("A", "D");
+        graph.addEdge("A", "C");
+        graph.addEdge("A", "B");
+        graph.printGraph();
+    }
+
+    public Graph<String, Integer> createGraphWithNodes() {
+        Graph<String, Integer> graph = new Graph<>();
+        String[] keys = {"A", "B", "C", "D", "F"};
+        Integer[] values = {1, 2, 3, 1, 2};
+        graph.addNodes(keys, values);
+        return graph;
+    }
+
+    public Graph<String, Integer> createGraphWithConnectedNodes() {
+        Graph<String, Integer> graph = new Graph<>();
+        String[] keys = {"A", "B", "C", "D", "F", "G"};
+        String[] aEdges = {"B", "C", "D", "F"};
+        Integer[] values = {1, 2, 3, 1, 2, 7};
+        graph.addNodes(keys, values);
+        graph.addEdges("A", aEdges);
+        graph.addEdge("B", "C");
+        graph.addEdge("C", "F");
+        graph.addEdge("D", "F");
+        graph.addEdge("F", "B");
+        return graph;
+    }
 }
